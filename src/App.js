4f6c9d96d88e986531878eac689a14ec4ea73f46
Tatiana Correia <taty.correia.s29@gmail.com>
2021-01-12 20:17:58 -0300

    desenvolvendo-inicio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>challenge-instructor-VnW</h1>
-    </div>
-  );
+import React, { Component } from 'react'
+import axios from 'axios';
+import './App.css'
+
+const api = axios.create({
+  baseURL: "http://newsapi.org/v2/everything?q=bitcoin&from=2020-12-12&sortBy=publishedAt&apiKey=719dc29ce5444befa2dd48895e99d30a"
+})
+
+class App extends Component{
+
+  state={
+    news: []
+
+  }
+
+  async componentDidMount() {
+    const response = await api.get();
+    console.log(response.data.articles)
+    
+    this.setState({
+      news: response.data.articles
+    });
+  }
+
+
+
+
+
+
+
+  render(){
+    return(
+      <div>
+        
+        <header className="menu">
+          <h1>Tudum</h1>
+          <button>Facebook</button>
+          <button>twitter</button>
+        </header>
+        <div className="box-map">
+          {this.state.news.map((item, index) => (
+            <div className="box-return-map" key={index}>
+              <h2>{item.author}</h2>
+              <p>{item.title}</p>
+              <img url={item.urlToImage} alt=""/>
+            </div>
+        ))}
+
+        </div>
+      </div>
+      
+       
+    )
+  }
 }
 
 export default App;
