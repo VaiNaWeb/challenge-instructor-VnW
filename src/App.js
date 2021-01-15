e796a54d00d44ba42bd3ee1a751bd2c536edbdf5
Bruna Rangel <bruna.henrique.rangel@gmail.com>
2021-01-15 16:04:17 -0300

    adding header and buttons

--- src/App.js
+++ src/App.js
@@ -7,12 +7,33 @@ import Twitter from './assets/img/Twitter.png'
 import Tudum from './assets/img/tudum.png'
 import Quiz from './assets/img/quiz.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>challenge-instructor-VnW</h1>
-    </div>
-  );
+const apiNews = axios.create({
+  baseURL: "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=df831af7bbd9429aa11f04376e696e6b"
+});
+
+
+class App extends Component{
+  render() {
+    return(
+      <section className="container">
+        <div className="container_header">
+          <div className="box_image_header">
+            <img className="image_logo" src={Logo} alt="Logo Tudum"/> 
+          </div>
+          <div className="box_btn_header">
+            <button className="btn">
+              <img  className="img_btn" src={Face} alt="" />
+              <p className="text_btn">CURTIR</p>
+            </button>
+            <button className="btn">
+              <img className="img_btn" src={Twitter} alt="" />
+              <p className="text_btn">SEGUIR</p>
+            </button>
+          </div>
+        </div>  
+      </section>      
+    );
+  }
 }
 
 export default App;
