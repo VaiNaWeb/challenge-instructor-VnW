48c720fbb5c507c70e251e0e755a006659e8c694
Tatiana Correia <taty.correia.s29@gmail.com>
2021-01-15 20:54:49 -0300

    desafio-finalizado

--- src/App.js
+++ src/App.js
@@ -57,61 +57,62 @@ class App extends Component{
             </div>
           ))}
         </div>
+        <h2 className="title2">THE GOOD PLACE</h2>
         <div className="box-map2">
-          <h2 className="title2">THE GOOD PLACE</h2>
-          {this.state.news.slice(1,4).map((item, index) => (
+            {this.state.news.slice(1,4).map((item, index) => (
             <div className="box-return-map2" key={index}>
-              <img className="img-news" src={item.urlToImage} alt="" />
-              <div className="box-descricao-meio">
-                <p className="title-img">{item.title}</p>
-              </div>
-                         
+              <img className="img-news2" src={item.urlToImage} alt="" />              
+              <p className="title-img">{item.title}</p>                         
             </div>
           ))}
         </div>
-        <div className="box-map3">
-          <h2 className="title3">THE GOOD PLACE</h2>
+        <h2 className="title3">THE GOOD PLACE</h2>
+        <div className="box-map3">          
           {this.state.news.slice(4,10).map((item, index) => (
             <div className="box-return-map3" key={index}>
-              <img className="img-news-meio3" src={item.urlToImage} alt="" />
-              <div className="box-descricao3">
-                <p className="title">{item.title}</p>
+              <img className="img-news-meio3" src={item.urlToImage} alt="" />        
+              <p className="title-img3">{item.title}</p>
+              <p className="descricao-3">{item.description}</p>                        
+            </div>
+          ))}
+        </div>
+        <div className="box-final">
+          <div className="box-map4">
+            <h2 className="title4">THE GOOD PLACE</h2>
+            {this.state.news.slice(10,16).map((item, index) => (
+              <div className="box-return-map" key={index}>
+                <div className="box-descricao4">
+                  <p className="descricao4">{item.description}</p>
+                  <div className="linha"></div>
+                </div>                         
               </div>
-                         
-            </div>
-          ))}
+            ))}
+          </div>
+          <div className="box-map5">
+            <h2 className="title5">THE GOOD PLACE</h2>
+            {this.state.news.slice(16,18).map((item, index) => (
+              <div className="box-return-map5" key={index}>
+                <img className="img-news-meio5" src={item.urlToImage} alt="" />              
+                <div className="box-descricao5">
+                  <p className="title-5">{item.title}</p>
+                  <p className="descricao5">{item.description}</p>
+                </div>                         
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="box-map4">
-          <h2>THE GOOD PLACE</h2>
-          {this.state.news.slice(10,16).map((item, index) => (
-            <div className="box-return-map" key={index}>
-              <div className="box-descricao4">
-                <p className="descricao4">{item.description}</p>
-              </div>                         
-            </div>
-          ))}
-        </div>
-        <div className="box-map5">
-          <h2>THE GOOD PLACE</h2>
-          {this.state.news.slice(16,18).map((item, index) => (
-            <div className="box-return-map5" key={index}>
-               <img className="img-news-meio5" src={item.urlToImage} alt="" />              
-              <div className="box-descricao5">
-                <p className="title-5">{item.title}</p>
-                <p className="descricao5">{item.description}</p>
-              </div>                         
-            </div>
-          ))}
-        </div>
-        <footer>
-          <a href="#header">
+        <a href="#header">
           <p className="volta-topo">VOLTAR AO TOPO</p>
           </a>
-          <img className="logo-footer" src={logoFooter} alt= ""/>      
-          <button></button>
-          <button></button>
-          <button></button>
-        
+        <footer>
+          <div className="box-footer">
+            <img className="logo-footer" src={logoFooter} alt= ""/>  
+            <div className="buttons">
+              <button className="botao-redondo"></button>
+              <button className="botao-redondo"></button>
+              <button className="botao-oval"></button>
+            </div>
+          </div>      
         </footer>
 
       </div>
