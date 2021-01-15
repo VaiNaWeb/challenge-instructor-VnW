61b7c09354725320f1779d04403e09a32686a6eb
Bruna Rangel <bruna.henrique.rangel@gmail.com>
2021-01-15 15:48:44 -0300

    adding libs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
+import Logo from './assets/img/bbc.png'
+import Face from './assets/img/facebook.png'
+import Twitter from './assets/img/Twitter.png'
+import Tudum from './assets/img/tudum.png'
+import Quiz from './assets/img/quiz.jpg'
 
 function App() {
   return (
